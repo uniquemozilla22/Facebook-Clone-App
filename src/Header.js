1ec77301c84c53879f2f6b088b08c8a2718b6918
uniquemozilla22 <49606627+uniquemozilla22@users.noreply.github.com>
2020-09-20 10:29:25 +0545

    header left part is doing

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="header__left">
+                <img src="https://www.bing.com/th?id=AMMS_8ddf76a14a2e3ad3ba62b46d49a75a74&w=72&h=72&c=7&rs=1&qlt=80&cdv=1&dpr=1.25&pid=16.1" alt="Facebook-Clone"/>
+            </div>
+            <div className="header__middle"></div>
+            <div className="header__right"></div>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA8kjSL6thGEEGdNHyCgMt2f2lbLa6hwrM",
+  authDomain: "facebook-clone-35df7.firebaseapp.com",
+  databaseURL: "https://facebook-clone-35df7.firebaseio.com",
+  projectId: "facebook-clone-35df7",
+  storageBucket: "facebook-clone-35df7.appspot.com",
+  messagingSenderId: "962714274551",
+  appId: "1:962714274551:web:ec40c8767008bd2a19a1f2",
+  measurementId: "G-712ZLN52DF"
+};
