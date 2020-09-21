ed52c99b8a893b41494a57f1490798f905f576b8
uniquemozilla22 <49606627+uniquemozilla22@users.noreply.github.com>
2020-09-21 21:47:30 +0545

    imported sidebar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
-
+import Sidebar from './Sidebar'
 function App() {
   return (
     <div className="app">
     <Header/>
     <div className="app__body">
-      {/* Sidebar */}
+      <Sidebar/>
       {/* Feed */}
       {/* Widgets */}
     </div>
