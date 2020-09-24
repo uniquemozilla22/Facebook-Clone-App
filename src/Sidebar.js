d5e1c6e1de9f8c87ab38749ed9c654236bcf0bed
uniquemozilla22 <49606627+uniquemozilla22@users.noreply.github.com>
2020-09-24 01:09:22 -0800

    sidebar adjustments done

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,14 +1,25 @@
+import { Chat, EmojiFlags, People, Storefront, VideoLibrary ,ExpandMoreOutlined} from '@material-ui/icons'
 import React from 'react'
 import SidebarRow from'./SidebarRow.js'
 
-const Sidebar = (props) => {
+const Sidebar = () => {
     return (
         <div className="sidebar">
-            <SidebarRow title="friends"/>
-            <SidebarRow title="contactUs"/>
-            <SidebarRow title="DiscoverPeople"/>
-            <SidebarRow title="FindFriends"/>
-            <SidebarRow title="Play Games"/>  
+            <SidebarRow src={'https://www.yogesh-bhattarai.com/img/home-bg-2.jpg'} title={'Yogesh Bhattarai'}/>
+
+            <SidebarRow Icon={EmojiFlags}
+            title="Pages"/>
+            <SidebarRow Icon={People}
+            title="Friends"/>
+            <SidebarRow Icon={Chat}
+            title="Messenger"/>
+            <SidebarRow Icon={Storefront}
+            title="Marketplace"/>
+            <SidebarRow Icon={VideoLibrary}
+            title="COVID-19 INFORMATION CENTER"/>
+            <SidebarRow Icon={ExpandMoreOutlined}
+            title="Marketplace"/>
+
         </div>
     )
 }
--- src/SidebarRow.js
+++ src/SidebarRow.js
@@ -1,11 +1,14 @@
+import { Avatar } from '@material-ui/core'
 import React from 'react'
 
 import './SidebarRow.css'
 
-const SidebarRow = (props) => {
+const SidebarRow = (src,Icon , title) => {
     return (
         <div className="sidebarRow">
-            {props.title}
+            {src&&<Avatar src={src}/>}
+            {Icon && <Icon />}
+            <h4>{title}</h4>
         </div>
     )
 }
