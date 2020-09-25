f287f9801dbd503f2bcc21c7586fa1672208d6ce
uniquemozilla22 <49606627+uniquemozilla22@users.noreply.github.com>
2020-09-25 15:28:30 +0545

    added context API but not working

--- src/Login.js
+++ src/Login.js
@@ -2,16 +2,22 @@ import React from 'react'
 import './Login.css'
 import Logo from '././Logo/facebook-f-logo-white-background-21.jpg'
 import { Button } from '@material-ui/core'
-
+import actionTypes from './Reducer'
+import {useStateValue} from "./StateProvider"
 import {auth , provider} from './firebase'
 
 const Login = (props) => {
+
+    const [state,dispatch] =useStateValue();
 
     const SignIn=()=>{
 
         auth.signInWithPopup(provider).then((result)=>
         {
-            console.log(result);
+            dispatch({
+                type:actionTypes.SET_USER,
+                user:result.user
+            })
 
         })
         .catch((error)=>alert(error.message))
